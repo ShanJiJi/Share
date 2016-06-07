967499fcc38a0aabbb816db4817d85395e1d99bf
ShanJiJi <752536885@qq.com>
2016-06-07 18:45:25 +0800

    添加了微信分享功能
    添加了分享到朋友和朋友圈功能

--- Share/AppDelegate.m
+++ Share/AppDelegate.m
@@ -7,8 +7,12 @@
 //
 
 #import "AppDelegate.h"
+#import "WXApi.h"
 
-@interface AppDelegate ()
+//注册应用是的APPID
+#define WXAPPID  @"wxb9bd182722d85fae"
+
+@interface AppDelegate ()<WXApiDelegate>
 
 @end
 
@@ -16,7 +20,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    //注册微信
+    [WXApi registerApp:WXAPPID];
+    
     return YES;
 }
 
@@ -41,5 +48,75 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+-(BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options{
+    
+    
+    return [WXApi handleOpenURL:url delegate:self];
+}
+
+#pragma mark - WXPaiDelegate 微信分享的相关回调
+
+//onReq是微信终端向第三方程序发起请求，要求第三方程序相应。第三方程序响应完后必须调用sendRsp返回。在调用sendRsp返回时，会切回到微信终端程序界面。
+-(void)onReq:(BaseReq *)req{
+    
+}
+
+/** 如果第三方程序向微信发送了sendReq的请求，那么onResp会被回调。sendReq请求调用后，会切到微信终端程序界面。*/
+/**
+ enum  WXErrCode {
+ WXSuccess           = 0,    //< 成功
+WXErrCodeCommon     = -1,   //< 普通错误类型
+WXErrCodeUserCancel = -2,   //< 用户点击取消并返回
+WXErrCodeSentFail   = -3,   //< 发送失败
+WXErrCodeAuthDeny   = -4,   //< 授权失败
+WXErrCodeUnsupport  = -5,   //< 微信不支持
+};*/
+
+-(void)onResp:(BaseResp *)resp{
+    if([resp isKindOfClass:[SendMessageToWXResp class]]) {
+        
+        switch (resp.errCode) {
+            case WXSuccess:
+            {
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"成功" message:@"微信分享成功" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+                [alert show];
+            }
+                break;
+            case WXErrCodeUserCancel:
+            {
+                NSLog(@"用户点击取消并返回");
+            }
+                break;
+            case WXErrCodeCommon:
+            {
+                NSLog(@"普通错误类型");
+            }
+                break;
+            case WXErrCodeSentFail:
+            {
+                NSLog(@"发送失败");
+            }
+                break;
+            case WXErrCodeAuthDeny:
+            {
+                NSLog(@"授权失败");
+            }
+                break;
+            case WXErrCodeUnsupport:
+            {
+                NSLog(@"微信不支持");
+            }
+                break;
+            default:
+            {
+                
+            }
+                
+                break;
+        }    
+    }
+}
+
 
 @end
--- Share/ViewController.m
+++ Share/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
 
 @interface ViewController ()
 
@@ -18,10 +20,72 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)shareClick:(id)sender {
+    
+    [self SendTextImageLink:NO scene:0];
 }
+- (IBAction)share2Click:(id)sender {
+    
+    [self SendTextImageLink:NO scene:1];
+}
+- (IBAction)share3:(id)sender {
+    
+    [self SendTextImageLink:YES scene:0];
+}
+- (IBAction)share4:(id)sender {
+    
+    [self SendTextImageLink:YES scene:1];
+}
+
+//bText = YES表示使用文本信息 NO表示不使用文本信息
+//scene = 0：分享到好友列表 1：分享到朋友圈  2：收藏
+-(void)SendTextImageLink:(BOOL)bText scene:(int)scene{
+    
+    //判断用户是否安装了微信
+    if (![WXApi isWXAppInstalled]) {
+        NSLog(@"没有安装，请去前去下载按装");
+    }else{
+        
+        //第三方程序向微信发送信息需要传入SendMessageToWXReq结构体，信息类型包括文本消息和多媒体消息
+        SendMessageToWXReq *sendReq = [[SendMessageToWXReq alloc] init];
+        
+        //发送消息的类型，包括文本消息和多媒体消息两种，两者只能选择其一，不能同时发送文本和多媒体消息
+        sendReq.bText = bText;//YES表示使用文本信息 NO表示不使用文本信息
+        
+        //判断只发文本还是发图片和文本
+        if (bText) {
+            sendReq.text = @"这是分享测试，，测试，，";
+        }else{
+            
+            //创建分享内容
+            WXMediaMessage *message = [WXMediaMessage message];
+            //分享标题
+            message.title = @"这是标题";
+            //分享描述，或内容
+            message.description = @"这是内容描述！这是内容描述！这是内容描述！这是内容描述！这是内容描述！这是内容描述！这是内容描述！";
+            //分享的图片
+            [message setThumbImage:[UIImage imageNamed:@"这里写要发送的图片名"]];//分享图片,使用SDK的setThumbImage方法可压缩图片大小
+            
+            //创建多媒体对象
+            WXWebpageObject *webObj = [WXWebpageObject object];
+            //点击后的跳转链接
+            webObj.webpageUrl = @"www.baidu.com";
+            //多媒体数据对象，可以为WXImageObject，WXMusicObject，WXVideoObject，WXWebpageObject等。
+            message.mediaObject = webObj;
+            //发送消息的多媒体内容
+            sendReq.message = message;
+            
+        }
+        
+        //发送的目标场景，可以选择发送到会话(WXSceneSession)或者朋友圈(WXSceneTimeline)。 默认发送到会话。
+        sendReq.scene = scene;// 0：分享到好友列表 1：分享到朋友圈  2：收藏
+                //发送请求到微信
+        [WXApi sendReq:sendReq];
+        
+    }
+    
+}
+
+
 
 @end
