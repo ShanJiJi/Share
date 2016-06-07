e4fbc061ce72ae29284361c73003e5810fc365d4
ShanJiJi <752536885@qq.com>
2016-06-07 14:23:43 +0800

    Initial Commit

---
+++ Share/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Share
+//
+//  Created by 珍玮 on 16/6/7.
+//  Copyright © 2016年 ZhenWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Share/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Share
+//
+//  Created by 珍玮 on 16/6/7.
+//  Copyright © 2016年 ZhenWei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
